db60835db083a092edd6d7ebffac3af458c23a26
Kristina Ledinski <komarcica@gmail.com>
2022-11-03 16:23:54 +0100

    js: error style for border

--- validate.js
+++ validate.js
@@ -2,6 +2,7 @@ function validateEmail(text) {
     let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!text.value.match(emailFormat)) {
         document.getElementById("message").innerHTML = "Please provide a valid email";
+        document.getElementById("email").style.border = "3px solid var(--softred)";
     }
     else {
         document.getElementById("message").innerHTML = "";
